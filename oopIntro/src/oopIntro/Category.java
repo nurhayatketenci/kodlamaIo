821bf3b056b5299420021a15b5c170a185b7df8c
Nurhayat <69644512+nurhayatketenci@users.noreply.github.com>
2021-04-29 01:11:09 +0300

    commit

---
+++ oopIntro/src/oopIntro/Category.java
@@ -0,0 +1,18 @@
+package oopIntro;
+
+public class Category {
+    private int id;
+    private String name;
+    public int getId() {
+    	return this.id;
+    }
+    public void setId(int id) {
+    	this.id=id;
+    }
+    public String getName() {
+    	return this.name+" ! ";
+    }
+    public String setName(String name) {
+    	return this.name=name;
+    }
+}
---
+++ oopIntro/src/oopIntro/Main.java
@@ -0,0 +1,25 @@
+package oopIntro;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Product product1 = new Product(1, "Lenovo V14", 15000, "16 GB Ram",10);
+		
+		Product product2=new Product();
+		product2.setId(2);
+		product2.setName("lonovo v15");
+		product2.setDetail("16 gb ram");
+		product2.setDicount(10);
+		product2.setUnitPrice(16000);
+		System.out.println(product2.getUnitPriceAfterDiscount());
+		
+        Category category1=new Category();
+        category1.setId(1);
+       category1.setName("içecek");
+        
+        System.out.println(category1.getName());
+	}
+
+}
