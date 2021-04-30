311990b7c037bbd326fef6037990be955d159c58
Nurhayat <69644512+nurhayatketenci@users.noreply.github.com>
2021-04-30 03:18:46 +0300

    commit2

--- kodlama.io/src/kodlama/io/UserManager.java
+++ kodlama.io/src/kodlama/io/UserManager.java
@@ -3,30 +3,26 @@ package kodlama.io;
 public class UserManager {
 
 	public void add( User user) {
-		System.out.println(user.getFirstName() + " kullanýcýsý eklendi.");
+		System.out.println(user.getFirstName() + "  eklendi.");
 	}
 
 	public void delete( User user) {
-		System.out.println(user.getFirstName() + " kullanýcýsý silindi.");
+		System.out.println(user.getFirstName() + "  silindi.");
 	}
 
 	public void getAll() {
-		System.out.println("Tüm kullanýcýlar listelendi.");
+		System.out.println("kullanýcýlar listelendi.");
 	}
-
-
-
-	public void login( String email,  String password) {
-		System.out.println(email + " e-postaya sahip kullanýcý tarafýndan giriþ yapýldý.");
+    public void login( String email,  String password) {
+		System.out.println(email + "giriþ yapýldý.");
 	}
 
 	public void logout( User user) {
-		System.out.println(
-				user.getEmail() + " e-postaya sahip " + user.getFirstName() + "kullanýcý tarafýndan çýkýþ yapýldý.");
+		System.out.println(user.getFirstName() + "çýkýþ yapýldý.");
 	}
 
 	public void update( User user) {
-		System.out.println(user.getFirstName() + " kullanýcýsý güncellendi.");
+		System.out.println(user.getFirstName() + "güncellendi.");
 	}
 	
 	
